be6c0aa8e2162b94f7f17cdc19933a5f8fe5f8a3
Isamun <erik.s.haugstad@gmail.com>
2014-05-06 16:47:40 +0200

    yay

---
+++ Networking/Client.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking.Sockets;
+using Windows.Networking;
+using Windows.Networking.Connectivity;
+using Windows.Networking.NetworkOperators;
+using Windows.Networking.BackgroundTransfer;
+using System.IO;
+
+namespace Networking
+{
+    public interface IClient
+    {
+        event DisconnectedHandler Disconnected;
+        event MessageReceivedHandler MessageReceived;
+        event MessageSubmittedHandler MessageSubmitted;
+
+        public delegate void MessageReceivedHandler(Client N, String Msg);
+        public delegate void MessageSubmittedHandler(Client N, String Msg);
+        public delegate void DisconnectedHandler(Client N);
+
+        async void StartClient();
+        void StartClient(String Hostname, String Port);
+        bool IsConnected();
+        async void Receive();
+        void Send(String msg, bool close);
+        void close();
+
+    }
+
+    public class Client : IClient
+    {
+        private StreamSocket _socket;
+        private static HostName _localHost;
+        private HostName _remoteHost;
+        private String _serviceName;
+        private EndpointPair _endpointpair;
+
+
+        public Client()
+        {
+            _socket = new StreamSocket();
+            _localHost = new HostName("192.168.1.113");
+            _remoteHost = new HostName("192.168.1.145");
+            _serviceName = "1337";
+            _endpointpair = new EndpointPair(_localHost, _serviceName, _remoteHost, _serviceName);
+
+        }
+
+        public event IClient.DisconnectedHandler Disconnected;
+
+        public event IClient.MessageReceivedHandler MessageReceived;
+
+        public event IClient.MessageSubmittedHandler MessageSubmitted;
+
+        async public void StartClient()
+        {
+            await _socket.ConnectAsync(_endpointpair);
+        }
+
+        public void StartClient(string Hostname, string Port)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsConnected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Receive()
+        {
+
+        }
+
+        public void Send(string msg, bool close)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void close()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SocketThingy/MainPage.xaml.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Networking.Sockets;
+using Windows.Networking;
+using Newtonsoft.Json.Linq;
+using Demo.Protocol;
+using Windows.Storage.Streams;
+using System.Collections.ObjectModel;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SocketThingy
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        StreamSocket socket = new StreamSocket();
+        static HostName localHost = new HostName("158.37.233.19");
+        static HostName remoteHost = new HostName("158.37.233.16");
+        static string socketString = "1337";
+        DataReader dr2;
+        EndpointPair connection = new EndpointPair(localHost, socketString, remoteHost, socketString);
+        // ip 158.37.225.197
+        // ip 158.37.225.206
+        PDU pdu = new PDU()
+        {
+            MessageID = (int)CommandMessageID.SendAllProcedures,
+            MessageDescription = "Server Please, load the sequencefile I specified as part of this message.",
+            MessageType = "Command",
+            Source = "Demo.Client",
+            Data = new JObject()
+        };
+        public static bool DEBUG = true;
+
+        // StreamSocketListener tcpListener = new StreamSocketListener();
+        // private List<StreamSocket> _connections = new List<StreamSocket>();
+        private bool connecting = false;
+        private String recievedText;
+        public String RecievedText
+        {
+            get
+            {
+                return recievedText;
+            }
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            
+
+        }
+        static public ObservableCollection<kakeclass> _kakecollection = new ObservableCollection<kakeclass>();
+        // message fikk -> PDU object, 
+        public ObservableCollection<kakeclass> kakecollection
+        {
+            get { return _kakecollection; }
+        }
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+       
+
+        async private void recieveData()
+        {
+            StreamSocketListener listener = new StreamSocketListener();
+            DataReader dr = new DataReader(socket.InputStream);
+            dr.InputStreamOptions = InputStreamOptions.Partial;
+            string msg = null;
+            var count = await dr.LoadAsync(512);
+
+            if (count > 0)
+                msg = dr.ReadString(count);
+            recievedMessage.Text = msg;
+
+            dr.DetachStream();
+            dr.Dispose();
+
+            try
+            {
+                PDU pdu2 = new PDU(msg);
+                Procedure temp = pdu2.Data.ToObject(typeof(Procedure));
+                _kakecollection.Add(new kakeclass { Name = temp.Name, Description = temp.Description, Date = temp.Date });
+            }
+            catch (Exception e) { recievedMessage.Text = "Failed to cast incomming message to usable type: PDU"; }
+
+            try
+            {
+                listener.ConnectionReceived += listener_ConnectionReceived;
+                await listener.BindServiceNameAsync(socketString);
+                
+
+            }
+            catch (Exception e) { recievedMessage.Text = e.ToString(); }
+
+        }
+
+        void listener_ConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+            dr2 = new DataReader(socket.InputStream);
+            PDU pdu2 = new PDU(dr2.ToString());
+            Procedure temp = pdu2.Data.ToObject(typeof(Procedure));
+            _kakecollection.Add(new kakeclass { Name = "123", Description = temp.Description, Date = temp.Date });
+        }
+        private async void sendData()
+        {
+            var dr = new DataWriter(socket.OutputStream);
+            
+            //var len = dr.MeasureString(pdu.ToJson());
+            String message = pdu.ToJson();
+            //dr.WriteInt32((int)len);
+            dr.WriteString(pdu.ToJson());
+            var ret = await dr.StoreAsync();
+            
+            recieveData();
+
+
+            dr.DetachStream();
+          
+
+            
+        }
+
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DEBUG)
+            {
+                socket.Dispose();
+                
+                Application.Current.Exit();
+            }
+            else
+            {
+                // do something legal
+            }
+        }
+
+
+
+        private void RunTest_Click(object sender, RoutedEventArgs e)
+        {
+            SequencePage Seq = new SequencePage(gridMainPage);
+            gridMainPage.Children.Add(Seq);
+        }
+
+
+
+        private void ListViewItem_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            SequencePage Seq = new SequencePage(gridMainPage);
+            gridMainPage.Children.Add(Seq);
+        }
+
+        private void collectkake_Click(object sender, RoutedEventArgs e)
+        {
+            _kakecollection.Add(new kakeclass { Name = "entotre", Description = "send", Date = DateTime.Now });
+            //_kakecollection.Add(new kakeclass("navn", DateTime.Now, "to"));
+        }
+
+        private void sendText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private async void Coonnect3_Click(object sender, RoutedEventArgs e)
+        {
+            try { await socket.ConnectAsync(connection); }
+            catch {
+                
+                recievedMessage.Text = "Could not connect"; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            sendData();
+        }
+
+        private void SendData_Click(object sender, RoutedEventArgs e)
+        {
+            sendData();
+        }
+        }
+    }
+
